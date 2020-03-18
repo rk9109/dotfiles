ca2735a766f95333e9a8a5f7c695dcb11cab10ba
Rinik Kumar <rk9109@gmail.com>
2020-03-18 01:31:29 -0400

    update plugins.zsh

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,16 +2,16 @@
 # zsh plugins
 #
 
-# install zplugin
-if [[ ! -d ~/.zplugin ]]; then
-    mkdir $HOME/.zplugin
-    git clone https://github.com/zdharma/zplugin.git $HOME/.zplugin/bin
+# install zinit
+if [[ ! -d ~/.zinit ]]; then
+    mkdir $HOME/.zinit
+    git clone https://github.com/zdharma/zinit.git $HOME/.zinit/bin
     # prevent error:
     #   "zsh compinit: insecure directories"
-    sudo chown -R rinik:root $HOME/.zplugin
-    sudo chmod -R 755 $HOME/.zplugin
+    sudo chown -R rinik:root $HOME/.zinit
+    sudo chmod -R 755 $HOME/.zinit
 fi
-source $HOME/.zplugin/bin/zplugin.zsh
+source $HOME/.zinit/bin/zinit.zsh
 
 
 # regenerate .zcompdump periodically
@@ -28,11 +28,11 @@ function bettercompinit() {
 
 
 # load plugins
-zplugin ice wait'0a' lucid
-zplugin load zsh-users/zsh-autosuggestions
+zinit ice wait'0a' lucid
+zinit load zsh-users/zsh-autosuggestions
 
-zplugin ice wait'0b' lucid atinit"bettercompinit"
-zplugin load zsh-users/zsh-syntax-highlighting
+zinit ice wait'0b' lucid atinit"bettercompinit"
+zinit load zsh-users/zsh-syntax-highlighting
 
 #
 # plugin configuration
